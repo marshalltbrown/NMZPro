23ee9082d2391b04e30f31cc8040fcc27fbabde0
Marshall Brown <82731225+marshalltbrown@users.noreply.github.com>
2021-09-13 00:52:13 -0700

    Shortened time between prayer flick clicks. .8 delay starts can potentially gain prayer. THis should be a sweet spot.

--- Actions.py
+++ Actions.py
@@ -169,7 +169,7 @@ def flickRapidHeal(client, string_dict, lock):
 
         # Flick prayer.
         pyautogui.click()
-        time.sleep(getSleepTRNV(.8))
+        time.sleep(getSleepTRNV(.6))
         pyautogui.click()
 
         # Move off screen
